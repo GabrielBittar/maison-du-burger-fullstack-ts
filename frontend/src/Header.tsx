50a07c3db002ae046b0cfd9c729fb036d5cf85ac
GabrielBittar <gabrielbittardomingues@gmail.com>
2025-11-30 00:29:42 -0400

    feature: add initial header component

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <div className="bg-[#161410]">
+            <div className="w-[737px] mx-auto flex items-center justify-between">
+                <img src="./provisory-logo.png" alt="" />
+                <div className="bg-[#F2DACC] w-[130px] h-[35px] flex items-center justify-center rounded-sm cursor-pointer">
+                    Se Connecter
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
