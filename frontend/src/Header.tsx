8c6f915758210303ef8768ae14c135b2f6195b09
GabrielBittar <gabrielbittardomingues@gmail.com>
2025-11-30 14:26:27 -0400

    set up header responsiveness

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className="bg-[#161410]">
-            <div className="w-[737px] mx-auto flex items-center justify-between">
+            <div className="w-full md:w-[737px] p-3 md:p-0 mx-auto flex items-center justify-between">
                 <img src="./provisory-logo.png" alt="" />
                 <div className="bg-[#F2DACC] w-[130px] h-[35px] flex items-center justify-center rounded-sm cursor-pointer">
                     Se Connecter
