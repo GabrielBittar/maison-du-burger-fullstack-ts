fbf3a832d176057e68f4c9e33660fb188917eb9d
GabrielBittar <gabrielbittardomingues@gmail.com>
2025-12-05 15:58:17 -0400

    add button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,9 +1,9 @@
 type ButtonType = {
   title: string;
   variant?: "default" | "outline";
-};
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ title, variant }: ButtonType) => {
+const Button = ({ title, variant = "default", ...props }: ButtonType) => {
   const buttonVariant = () => {
     if (variant === "default") {
       return "w-full cursor-pointer rounded-md border-2 border-[#C92A0E] bg-[#C92A0E] py-2 text-sm font-bold text-white";
@@ -12,6 +12,10 @@ const Button = ({ title, variant }: ButtonType) => {
     }
   };
 
-  return <button className={buttonVariant()}>{title}</button>;
+  return (
+    <button {...props} className={buttonVariant()}>
+      {title}
+    </button>
+  );
 };
 export default Button;
