b0c48bc85f8e673d075cd10d593e4fc0df3dab23
GabrielBittar <gabrielbittardomingues@gmail.com>
2025-12-27 14:35:22 -0400

    feature: insert try-catch block on login error treatment

--- backend/index.ts
+++ backend/index.ts
@@ -7,21 +7,26 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    res.status(400).json({ message: "E-mail and password are required." });
+    if (!email || !password) {
+      res.status(400).json({ message: "E-mail and password are required." });
+    }
+
+    const user = await prisma.user.findFirst({
+      where: { email, password },
+    });
+
+    if (!user) {
+      res.status(404).json({ message: "User not found." });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error." });
+    return;
   }
-
-  const user = await prisma.user.findFirst({
-    where: { email, password },
-  });
-
-  if (!user) {
-    res.status(404).json({ message: "User not found." });
-  }
-
-  res.status(200).json(user);
 });
 
 app.listen(3000, () => {
