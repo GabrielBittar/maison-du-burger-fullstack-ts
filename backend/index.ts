8280d684a7ef3e306575311e84f6bc28000ce691
GabrielBittar <gabrielbittardomingues@gmail.com>
2025-12-29 22:32:41 -0400

    add signup endpoint structure and correct user table schema

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { prisma } from "./src/db";
 import cors from "cors";
 
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/login", async (req, res) => {
+app.post("/login", async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
 
@@ -23,6 +23,41 @@ app.post("/login", async (req, res) => {
     }
 
     res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error." });
+    return;
+  }
+});
+
+app.post("/register", async (req: Request, res: Response) => {
+  try {
+    const { name, email, password, postalCode } = req.body;
+
+    if (!name || !email || !password || !postalCode) {
+      res.status(400).json({ message: "All fields must be completed." });
+      return;
+    }
+
+    const user = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (user?.email) {
+      res.status(409).json({ message: "E-mail already registered" });
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+        postalCode: postalCode,
+      },
+    });
+
+    res.status(201).json(newUser);
   } catch (error) {
     res.status(500).json({ message: "Server error." });
     return;
