36ab073c5020c2556a842a3063c3735a7d6ac903
GabrielBittar <gabrielbittardomingues@gmail.com>
2025-12-30 23:54:49 -0400

    feature: add frontend error treatment on user register form

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -9,6 +9,7 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [postalCode, setPostalCode] = useState("");
+  const [error, setError] = useState("");
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -19,6 +20,29 @@ const Register = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password, postalCode }),
       });
+
+      switch (response.status) {
+        case 409:
+          setError("Email déjà enregistré");
+          break;
+        case 400:
+          setError("Toutes les informations sont requises");
+          break;
+        case 201:
+          setName("");
+          setEmail("");
+          setPassword("");
+          setConfirmPassword("");
+          setPostalCode("");
+          setError("");
+          break;
+        case 500:
+          setError("Une erreur est survenue. Veuillez réessayer plus tard");
+          break;
+        default:
+          setError("");
+          break;
+      }
       console.log(response);
     } catch (error) {
       console.log(error);
@@ -29,9 +53,9 @@ const Register = () => {
       onSubmit={handleSubmit}
       className="flex h-screen items-center justify-center bg-[#161410]"
     >
-      <div className="flex flex-col items-center justify-center gap-2">
+      <div className="flex flex-col justify-center gap-2">
         <Link to="/">
-          <img src="./provisory-logo.png" alt="" className="mb-4" />
+          <img src="./provisory-logo.png" alt="" className="mx-auto mb-4" />
         </Link>
         <Input
           placeholder="Nom et prénom"
@@ -57,6 +81,7 @@ const Register = () => {
           type="text"
           onChange={(e) => setPostalCode(e.target.value)}
         ></Input>
+        <p className="font-bold text-red-500">{error}</p>
         <div className="mt-2 flex w-full flex-col gap-2">
           <Button title="S'inscrire" type="submit" />
           <Link to="/login" className="w-full">
