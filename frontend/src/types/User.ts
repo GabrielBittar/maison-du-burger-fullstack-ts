fa719f0039aa76eaccfe9c4194f6201cf586b7dd
GabrielBittar <gabrielbittardomingues@gmail.com>
2026-01-07 23:40:34 -0400

    feature: implement UserContext and provider

--- frontend/src/types/User.ts
+++ frontend/src/types/User.ts
@@ -1,6 +1,11 @@
-export interface UserType {
+export interface UserInterface {
   id: string;
   name: string;
   email: string;
   postalCode: string;
 }
+
+export type UserContextType = {
+  user: UserInterface | null;
+  setUser: React.Dispatch<React.SetStateAction<null>>;
+};
